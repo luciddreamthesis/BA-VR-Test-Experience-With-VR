009cfe98b932474dd2bd2531f4acd7ebe4c4b975
luciddreamthesis <luciddreamthesis@gmail.com>
2020-06-15 14:29:26 +0200

    Add 3D Audio

---
+++ 2006015_Test_Experience_with_VR/Assets/_Scripts/CylinderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderController : MonoBehaviour
+{
+    public Transform B;
+    public bool isMoving = false;
+
+    float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        var step = speed * Time.deltaTime;
+        if (isMoving) ;
+        {
+            transform.position = Vector3.MoveTowards(transform.position, B.position, step);
+        }
+    }
+}
