dc119401186676094a2402994f476da6b8dd227f
luciddreamthesis <luciddreamthesis@gmail.com>
2020-06-26 14:51:26 +0200

    Add Transition Scene 1st to 2nd (FadeIn, FadeOut)

---
+++ 2006015_Test_Experience_with_VR/Assets/LevelChanger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("goToSecondScene", 3);
+    }
+
+    public void goToSecondScene ()
+    {
+        FadeToLevel(1);
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
