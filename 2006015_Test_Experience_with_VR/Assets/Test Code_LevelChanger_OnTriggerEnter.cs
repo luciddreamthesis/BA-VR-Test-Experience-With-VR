b1dc5c6fba5c579a61d7e50395393bd6a10d6566
luciddreamthesis <luciddreamthesis@gmail.com>
2020-06-26 17:03:33 +0200

    create test code LevelChanger OnTriggerEnter
    For testing the transition between level 2 and 3 with Trigger Object instead of Invoke

---
+++ 2006015_Test_Experience_with_VR/Assets/Test Code_LevelChanger_OnTriggerEnter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger2nd : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    public void goToThirdScene()
+    {
+        FadeToLevel(2);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
+
