1d976434f0cc78c9f2cf94c668a4ed27a38fbc30
luciddreamthesis <luciddreamthesis@gmail.com>
2020-07-14 17:13:10 +0200

    Add TriggerOnEnter, Chamge Moon Apperance

---
+++ 2006015_Test_Experience_with_VR/Assets/_Scripts/EventInvokationTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class EventInvokationTrigger : MonoBehaviour {
+
+    public bool EventInvokationTriggerActive = true;
+
+    [Header("Leave empty detect all collisions")]
+    public string ColliderTag;
+    
+    [Space(5)]
+    public UnityEvent OnTriggerEnterEvent;
+    [Space(5)]
+    public UnityEvent OnTriggerStayEvent;
+    [Space(5)]
+    public UnityEvent OnTriggerExitEvent;
+
+    void Start () {
+        if (ColliderTag == null)
+        {
+            ColliderTag = "other";
+        }
+    }
+	
+	private void OnTriggerEnter(Collider ColliderTag)
+
+    {
+        OnTriggerEnterEvent.Invoke();
+    }
+        
+    private void OnTriggerStay(Collider ColliderTag)
+    {
+        OnTriggerStayEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider ColliderTag)
+    {
+        OnTriggerExitEvent.Invoke();
+    }
+
+}
