ef9bef985fb650a40968030c18be21f38dbd3eb8
luciddreamthesis <luciddreamthesis@gmail.com>
2020-08-03 10:56:56 +0200

    Change Moon Apperinece add Standart Assets

---
+++ 2006015_Test_Experience_with_VR/Assets/_Scripts/NewMoonApperience.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMoonApperience : MonoBehaviour
+{
+    public GameObject moonObject;
+    //public bool activateme;
+
+    void Start()
+    {
+        moonObject.SetActive(false);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Moon");
+        moonObject.SetActive(true);
+    }
+}
+
