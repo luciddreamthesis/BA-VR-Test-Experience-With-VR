c3cdbbafa9cf933cea337031b541661b9f5e7e95
luciddreamthesis <luciddreamthesis@gmail.com>
2020-08-18 09:36:46 +0200

    Experements with Fade in

--- 2006015_Test_Experience_with_VR/Assets/_Scripts/instantiateRandomSpheres.cs
+++ 2006015_Test_Experience_with_VR/Assets/_Scripts/instantiateRandomSpheres.cs
@@ -8,8 +8,8 @@ public class instantiateRandomSpheres : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       for(int i = 0;i<1000;i++){
-       		GameObject s = GameObject.Instantiate(sphere, new Vector3(Random.Range(600f, 0f), Random.Range(0f, 0f), Random.Range(0f, 0f)), Quaternion.identity);
+       for(int i = 0;i<100000;i++){
+       		GameObject s = GameObject.Instantiate(sphere, new Vector3(Random.Range(600f, 0f), Random.Range(600f, 0f), Random.Range(600f, 0f)), Quaternion.identity);
        } 
     }
 
